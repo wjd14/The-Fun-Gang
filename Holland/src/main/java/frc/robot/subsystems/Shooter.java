f70656c289c81f0a72b5a277335b88ecbd12d936
wjd14 <will@ayerdoyles.org>
2020-02-26 16:31:23 -0500

    shooter stuff

---
+++ Holland/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+
+/**
+ * Add your docs here.
+ */
+public class Shooter extends Subsystem {
+
+  private WPI_VictorSPX Left;
+  private WPI_VictorSPX Right;
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public Shooter(){
+
+    Left = new WPI_VictorSPX(7)
+    Right = new WPI_VictorSPX(6)
+
+
+  }
+
+
+
+  
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+
+  public void spinLeft(double wheelSpeed){
+    Left.set(0.7)
+  }
+}
